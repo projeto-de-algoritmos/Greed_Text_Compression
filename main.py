4422a680ea2b3bb41a02b73d7f8e625a60b3d6eb
Pedro <190094486@aluno.unb.br>
2023-05-29 08:59:26 -0300

    Atualização da main

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from compression import *
 import sys
+from compression import HuffmanCoding
 
 if len(sys.argv) != 2:
     print("Uso: python script.py [compress | decompress]")
     sys.exit(1)
 
-
 operation = sys.argv[1]
-
 path = "texto.txt"
-
 h = HuffmanCoding(path)
 
 if operation == "compress":
     output_path = h.compress()
-    print("Compressed file path: " + output_path)
+    print("Arquivo comprimido: " + output_path)
 elif operation == "decompress":
     output_path = h.compress()
     decom_path = h.decompress(output_path)
-    print("Decompressed file path: " + decom_path)
+    print("Arquivo descomprimido: " + decom_path)
 else:
     print("Comando inválido. Use 'compress' ou 'decompress'.")
-    
-    ##
